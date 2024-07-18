cf43e9fc4c781a4d60ac596dbdf34a79435b58a0
sahil <sahilhussain1231@gmail.com>
2024-07-18 19:05:43 +0530

    updated package

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/Amazon-Cloned/",
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
 })
